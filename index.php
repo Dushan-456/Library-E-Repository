6ec802966cccf7d03290c3ccb568fb126412db56
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2026-03-05 21:41:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,18 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     exit;
 }
 
+// 15-Minute Session Timeout (900 seconds)
+$timeout_duration = 900;
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout_duration) {
+    // Session expired
+    session_unset();
+    session_destroy();
+    header("Location: login.php?msg=timeout");
+    exit;
+}
+$_SESSION['last_activity'] = time(); // Update last activity time
+
+
 $baseDir = 'E Resources';
 
 function getDirectoryContents($path) {
