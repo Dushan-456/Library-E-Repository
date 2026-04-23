de86f1d7806b69b6a7f6de0a755887bdc81f43e1
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2026-04-23 10:06:25 +0530

    Update admin_user_analytics.php

--- admin_user_analytics.php
+++ admin_user_analytics.php
@@ -168,8 +168,8 @@ function formatDuration($seconds) {
                 </ul>
             </nav>
             <div class="sidebar-footer">
-                <p>&copy; 2026 PGIM Library -V1.0</p>
-                <p>Developed by PGIM IT Unit</p>
+                <p>&copy; 2026 PGIM Library -V1.2</p>
+                <p>Developed by PGIM IT Unit - Dushan</p>
             </div>
         </aside>
 
