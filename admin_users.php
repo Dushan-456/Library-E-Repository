16069707ec76cd4f3c7a4d2ab8656901da8e5bfe
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2026-04-23 10:06:28 +0530

    Update admin_users.php

--- admin_users.php
+++ admin_users.php
@@ -143,8 +143,8 @@ $activePage = 'all_users';
                 </ul>
             </nav>
             <div class="sidebar-footer">
-                <p>&copy; 2026 PGIM Library -V1.0</p>
-                <p>Developed by PGIM IT Unit</p>
+                <p>&copy; 2026 PGIM Library -V1.2</p>
+                <p>Developed by PGIM IT Unit - Dushan</p>
             </div>
         </aside>
 
