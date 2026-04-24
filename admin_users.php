1fcf125ed63f17370ef15671e34784f2d4c50d9a
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2026-04-24 23:47:42 +0530

    Update admin_users.php

--- admin_users.php
+++ admin_users.php
@@ -137,6 +137,9 @@ $activePage = 'all_users';
                     <li class="<?= $activePage == 'activity' ? 'active' : '' ?>" onclick="window.location.href='admin_activity.php'">
                         <i class="fas fa-history"></i> <span>Library Analytics</span>
                     </li>
+                    <li class="<?= $activePage == 'reindex' ? 'active' : '' ?>" onclick="window.location.href='admin_reindex.php'">
+                        <i class="fas fa-database"></i> <span>Search Index</span>
+                    </li>
                     <li onclick="window.location.href='profile.php'">
                         <i class="fas fa-id-card"></i> <span>My Profile</span>
                     </li>
